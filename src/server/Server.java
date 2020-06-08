fe83b3e3ff1f0c5b6ba714442c20ba55bbf14bf2
elka74 <mikheeva@1454.ru>
2020-06-08 13:19:42 +0300

    HW_7

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,4 +1,73 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.Vector;
+
 public class Server {
+    private List<ClientHandler> clients;
+    private AuthService authService;
+
+    public Server() {
+        clients = new Vector<>();
+        authService = new SimpleAuthService();
+        ServerSocket server = null;
+        Socket socket;
+
+        final int PORT = 8189;
+
+        try {
+            server = new ServerSocket(PORT);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился ");
+                new ClientHandler(this, socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler c : clients) {
+            c.sendMsg(msg);
+        }
+    }
+
+    public void indMsg(ClientHandler nick, String nickadr, String msg) {
+        for (ClientHandler c : clients) {
+            if ((c.getNick()).equals(nickadr)) {
+                c.sendMsg(c.getNick() + ": " + msg);
+            }
+        }
+        System.out.println("Сообщение от пользователя  " + nick + " для " + nickadr + ": " + msg);
+    }
+
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+
+
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
+        clients.remove(clientHandler);
+    }
+
+    public AuthService getAuthService() {
+        return authService;
+    }
+
+
 }
